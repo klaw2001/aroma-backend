816fd32e3ec6e4d489ebc15da40a8652d525c02c
klaw <netke2611@gmail.com>
2023-10-24 20:30:02 +0530

    mvc folder setup & users crud

---
+++ models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstname:{
+        type:String,
+        default:null,
+    },
+    lastname:{
+        type:String,
+        default:null
+    },
+    email:{
+        type:String,
+        default:null
+    },
+    password:{
+        type:String,
+        default:null
+    },
+    contact:{
+        type:Number,
+        default:null
+    },
+    dateofbirth:{
+        type:Date,
+        default:null
+    },
+    gender:{
+        type: String,
+        enum: ['male', 'female', 'other'],
+        default: null
+    },
+    about:{
+        type:String,
+        default:null
+    },
+    avatar:{
+        type:String,
+        default:null
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'], 
+        default: 'user', 
+      },
+    // status:{
+    //     type:Number,
+    //     default:null
+    // }
+})
+
+export default mongoose.model("User",UserSchema)
